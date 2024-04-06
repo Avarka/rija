da27e85f77ce15f5e64e17b1e156a02ce0a94696
Vad Avar <vadavar7@gmail.com>
2024-04-06 14:28:58 +0200

    Project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,9 +17,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(),
+    ScreenTrackingService,
+    UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'rija-webkeret-202324',
+    appId: '1:836445688645:web:645b45e8221ad85f0e242c',
+    storageBucket: 'rija-webkeret-202324.appspot.com',
+    apiKey: 'AIzaSyCb7XUeBVH5CqBWWPzMnXxSXL9egWfMAM4',
+    authDomain: 'rija-webkeret-202324.firebaseapp.com',
+    messagingSenderId: '836445688645',
+    measurementId: 'G-82LFYQ23L5',
+  },
+};
